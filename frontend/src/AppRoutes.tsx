61b40ebd252dba80dd8d4c33b7c497440c079c14
rdx.develops <rdx.develops@gmail.com>
2024-05-28 13:05:39 +0530

    add HomePage/LandingPage and Footer

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,10 +1,11 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import HomePage from "./Pages/HomePage";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Layout>HOME PAGE</Layout>} />
+      <Route path="/" element={<Layout><HomePage/></Layout>} />
       <Route path="/user-profile" element={<Layout>USER PROFILE PAGE</Layout>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 import { ReactNode } from "react";
 
 type Props = {
@@ -10,9 +11,8 @@ function Layout({ children }: Props) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-
+      <Hero />
       <div className="container mx-auto flex-1 py-10">{children}</div>
-
       <Footer />
     </div>
   );
