387fc2ea4074455780205a00c599f3d0924c6a49
rdx.develops <rdx.develops@gmail.com>
2024-05-29 18:49:27 +0530

    complete create user functionality

--- backend/src/routes/myUser.route.ts
+++ backend/src/routes/myUser.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import myUserController from "../controllers/myUser.controller";
+import { jwtCheck } from "../middlewares/auth";
 
 const router = express.Router();
 
 //  /api/my/user
-router.post("/", myUserController.createCurrentUser);
+router.post("/", jwtCheck, myUserController.createCurrentUser);
 
 export default router;
--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import HomePage from "./Pages/HomePage";
+import AuthCallbackPage from "./Pages/AuthCallbackPage";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Layout><HomePage/></Layout>} />
+      <Route path="/auth-callback" element = {<AuthCallbackPage/>}/>
       <Route path="/user-profile" element={<Layout>USER PROFILE PAGE</Layout>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
